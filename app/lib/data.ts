34c701e88c5f9bfac8a51a1901b57cda32cccca2
Liefu Zhang <liefuzhangnz@gmail.com>
2025-08-30 16:36:54 +1200

    add invoices and search and pagination

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -84,7 +84,7 @@ export async function fetchCardData() {
   }
 }
 
-const ITEMS_PER_PAGE = 6;
+const ITEMS_PER_PAGE = 2;
 export async function fetchFilteredInvoices(
   query: string,
   currentPage: number
