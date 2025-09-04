31be1377b8144a30c78245629604a63a3b64d41d
Liefu Zhang <liefuzhangnz@gmail.com>
2025-09-04 12:24:12 +1200

    Fix config issue

--- auth.ts
+++ auth.ts
@@ -11,10 +11,7 @@ import PostgresAdapter from "@auth/pg-adapter";
 import { Pool } from "pg";
 
 const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DATABASE,
+  connectionString: process.env.POSTGRES_URL,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
